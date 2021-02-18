4bebee0a40069ec40fa56b05adc81db621aa59a6
algorythmist <dimitri@tecacet.com>
2021-02-17 18:46:41 -0800

    changed to pytest

--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -1,17 +1,8 @@
-import unittest
+from fintools import *
 
-from portfolio import *
-
-
-class PortfolioTestCase(unittest.TestCase):
-
-    def test_portfolio_return(self):
-        r = [1, 2, 3]
-        w = [3, 2, 1]
-        total_return = portfolio_return(w, r)
-        self.assertEqual(10, total_return)
-        self.assertEqual(10, portfolio_return([1, 2, 3], [3, 2, 1]))
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_portfolio_return():
+    r = [1, 2, 3]
+    w = [3, 2, 1]
+    total_return = portfolio_return(w, r)
+    assert np.equal(10, total_return)
+    assert np.equal(10, portfolio_return([1, 2, 3], [3, 2, 1]))
