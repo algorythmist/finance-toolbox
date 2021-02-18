f656a5756a4c1ef3cdc53f1a2e33f4bae6e52289
algorythmist <dimitri@tecacet.com>
2021-02-18 11:19:11 -0800

    fix path

--- fintools/industry_data.py
+++ fintools/industry_data.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-INDUSTRY_DATA_DIR = os.path.join(ROOT_DIR, '..\data\industry')
+INDUSTRY_DATA_DIR = os.path.join(ROOT_DIR, '../data/industry')
 
 
 def load_industry_data(filename):
