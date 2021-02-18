644aaff0a1eb7d53a578a557933c15796ce83d5a
algorythmist <dimitri@tecacet.com>
2021-02-17 18:57:53 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='finance-toolbox',
+    version='1.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/algorythmist/finance-toolbox',
+    license='MIT',
+    author='dimitri',
+    author_email='dimitri@tecacet.com',
+    description='Financial Toolbox'
+)
