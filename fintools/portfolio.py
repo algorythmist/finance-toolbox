8e0413a737596bd472b61eda9620b1b7155b8b6d
algorythmist <dimitri@tecacet.com>
2021-02-17 19:17:19 -0800

    added method

--- fintools/portfolio.py
+++ fintools/portfolio.py
@@ -3,9 +3,20 @@ import numpy as np
 
 def portfolio_return(weights, returns):
     """
-    Compute the portfolio return
+    Compute the portfolio return given an array of returns
     :param weights: the weights of the portfolio
     :param returns: the array of returns corresponding to each asset
-    :return: the return
+    :return: the portfolio return
     """
     return np.array(weights).T @ np.array(returns)
+
+
+def portfolio_volatility(weights, covariance):
+    """
+    Compute the portfolio volatility, given a covariance matrix
+    :param weights: the weights of the portfolio
+    :param covariance: the covariance matrix
+    :return: the portfolio volatility (variance)
+    """
+    np_weights = np.array(weights)
+    return (np_weights.T @ covariance @ np_weights) ** 0.5
--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -1,8 +1,18 @@
+import pytest
+
 from fintools import *
+
 
 def test_portfolio_return():
     r = [1, 2, 3]
     w = [3, 2, 1]
     total_return = portfolio_return(w, r)
     assert np.equal(10, total_return)
-    assert np.equal(10, portfolio_return([1, 2, 3], [3, 2, 1]))
+
+
+def test_portfolio_volatility():
+    w = [0.5, 0.5]
+    cov = [[2.5, 3.0],
+           [3.0, 10.0]]
+    volatility = portfolio_volatility(w, cov)
+    assert 2.150 == pytest.approx(volatility, 0.001)
