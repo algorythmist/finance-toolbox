27eab9cf64c36b6ae82596178a5685374e22c8ce
algorythmist <dimitri@tecacet.com>
2021-05-29 16:04:16 +0000

    added requirements and todo

--- fintools/factors.py
+++ fintools/factors.py
@@ -22,12 +22,12 @@ def french_fama_regression(portfolio_returns, start_index, end_index,
     fff = load_fff_returns_monthly()[start_index:end_index]
     excess_returns = portfolio_returns[start_index:end_index] - fff[['RF']].values
     excess_market = fff[['Mkt-RF']].copy()
-    exp_var = excess_market
-    exp_var['Alpha'] = 1
+    factors = excess_market
+    factors['Alpha'] = 1
     if regression_type is RegressionType.THREE_FACTOR or regression_type is RegressionType.FIVE_FACTOR:
-        exp_var['HML'] = fff[['HML']]
-        exp_var['SMB'] = fff[['SMB']]
+        factors['HML'] = fff[['HML']]
+        factors['SMB'] = fff[['SMB']]
     if regression_type is RegressionType.FIVE_FACTOR:
-        exp_var['RMW'] = fff[['RMW']]
-        exp_var['CMA'] = fff[['CMA']]
-    return sm.OLS(excess_returns, exp_var).fit()
+        factors['RMW'] = fff[['RMW']]
+        factors['CMA'] = fff[['CMA']]
+    return sm.OLS(excess_returns, factors).fit()
