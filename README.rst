bc1768202b51838be327c170e0b7253a7d04d48c
Dimitri Papaioannou <algorythmist@users.noreply.github.com>
2024-11-16 08:06:46 -0800

    replaced pandas with yfinance

--- README.rst
+++ README.rst
@@ -1,5 +1,12 @@
 *******
-finance-toolbox
+Finance Toolbox
 *******
 
 Python Utilities for FinTech applications
+------------------------------------------
+
+Install Jupyter Kernel
+
+.. code-block:: console
+
+    python -m ipykernel install --user --name finance-toolbox
--- tests/test_price_data.py
+++ tests/test_price_data.py
@@ -1,4 +1,3 @@
-import pytest
 import os
 import pandas as pd
 from datetime import datetime
@@ -15,19 +14,19 @@ def test_load_daily_prices():
 def test_download_daily_prices():
     download_daily_prices('SPY', datetime(2010, 1, 1), datetime(2018, 12, 31))
     quotes = pd.read_csv('SPY.2010-01-01_2018-12-31.csv')
-    assert len(quotes) == 2263
+    assert len(quotes) == 2265
     os.remove('SPY.2010-01-01_2018-12-31.csv')
 
 
 def test_download_prices_with_str_dates():
     download_daily_prices('SPY', '2010-01-01', '2018-12-31')
     quotes = pd.read_csv('SPY.2010-01-01_2018-12-31.csv')
-    assert len(quotes) == 2263
+    assert len(quotes) == 2265
     os.remove('SPY.2010-01-01_2018-12-31.csv')
 
 
 def test_download_prices_with_filename():
     download_daily_prices('SPY', '2010-1-1', '2018-12-31', filename='SPY.csv')
     quotes = pd.read_csv('SPY.csv')
-    assert len(quotes) == 2263
+    assert len(quotes) == 2265
     os.remove('SPY.csv')
